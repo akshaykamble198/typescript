c91a6a688192ea4661672d40857466655b14b0c5
Pratiksha Dhumal <pratikshadhumal2019@gmail.com>
2022-05-21 18:37:22 +0530

    MultidimentionalArray

---
+++ hello.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var msg = "Hello world pratiksha! 123!!";
+console.log(msg);
---
+++ hello.ts
@@ -0,0 +1,3 @@
+export{}
+let msg="Hello world pratiksha! 123!!"
+console.log(msg);
